f21903c5314eb637b7a1e9c4e04aed7bb5d6335d
Alexander Shepetko <a@shepetko.com>
2017-11-25 22:04:40 +0200

    Release 1.0

--- README.md
+++ README.md
@@ -3,13 +3,22 @@
 
 ## Changelog
 
+
+### 1.0 (2017-11-25)
+
+Support for PytSite-6.0.
+
+
 ### 0.2 (2017-09-06)
+
 Main part of code moved to the `auth_google` plugin.
 
 
 ### 0.1.1 (2017-08-27)
+
 `plugin.json` updated.
 
 
 ### 0.1 (2017-07-29)
+
 First release.
--- __init__.py
+++ __init__.py
@@ -6,10 +6,10 @@ __license__ = 'MIT'
 
 
 def _init():
-    from pytsite import assetman
+    from plugins import assetman
 
     # Assets
-    assetman.register_package(__name__, alias='google')
+    assetman.register_package(__name__)
     assetman.t_js(__name__ + '@**')
     assetman.js_module('pytsite-google', __name__ + '@js/google')
 
