abf4c47f2d8f3978a4741928afcf2381977c27ec
Alexander Shepetko <a@shepetko.com>
2017-12-13 17:21:55 +0200

    Release 1.1

--- __init__.py
+++ __init__.py
@@ -5,13 +5,10 @@ __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
 
-def _init():
+def plugin_load():
     from plugins import assetman
 
     # Assets
     assetman.register_package(__name__)
-    assetman.t_js(__name__ + '@**')
+    assetman.t_js(__name__)
     assetman.js_module('pytsite-google', __name__ + '@js/google')
-
-
-_init()
